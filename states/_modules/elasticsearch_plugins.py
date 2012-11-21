39672ef5f4113ae02c79389485d96181ff557e4b
Bruno Clermont <bruno.clermont@gmail.com>
2012-11-21 15:48:10 +0800

    add elasticsearch_plugins module + state

---
+++ states/_modules/elasticsearch_plugins.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+'''
+Elasticsearch plugins module
+'''
+
+import os
+import logging
+
+log = logging.getLogger(__name__)
+
+# from elasticsearch debian packages
+default_es_home = '/usr/share/elasticsearch'
+
+def list(es_home=None):
+    '''
+    Return list of installed plugins
+    '''
+    if es_home is None:
+        es_home = default_es_home
+    plugins_directory = os.path.join(es_home, 'plugins')
+    if not __salt__['file.directory_exists'](plugins_directory):
+        return []
+    return __salt__['file.find'](plugins_directory,
+                                 **{'type': 'd', 'print': 'name'})
+
+def _elasticsearch_plugins(command, argument, es_home=None):
+    if es_home is None:
+        es_home = default_es_home
+    cmdline = '{0}/bin/plugin -{1} {2}'.format(es_home, command,
+                                                         argument)
+    ret = __salt__['cmd.run_all'](
+        cmdline
+    )
+    if ret['retcode'] == 0:
+        return ret['stdout']
+    else:
+        return False
+
+def install(url, es_home=None):
+    '''
+    Uninstall an elasticsearch plugin
+    '''
+    return _elasticsearch_plugins('install', url, es_home)
+
+def uninstall(name, es_home=None):
+    '''
+    Install an elasticsearch plugin
+    '''
+    if es_home is None:
+        es_home = default_es_home
+    return _elasticsearch_plugins('remove', name, es_home)
---
+++ states/_states/elasticsearch_plugins.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# author: Bruno Clermont <patate@fastmail.cn>
+
+'''
+Elasticsearch plugins state
+'''
+
+def uninstalled(name, es_home=None):
+    '''
+    Make sure that a plugin is uninstalled.
+
+    name
+        The name of the plugin to disable
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+    if __opts__['test']:
+        ret['comment'] = 'The plugin {0} would have been uninstalled'.format(
+            name)
+        return ret
+
+    if name not in __salt__['elasticsearch_plugins.list'](es_home):
+        ret['result'] = True
+        ret['comment'] = 'Plugin is not installed.'
+        return ret
+
+    __salt__['elasticsearch_plugins.uninstall'](name, es_home)
+    if name not in __salt__['elasticsearch_plugins.list'](es_home):
+        ret['result'] = True
+        ret['changes'][name] = 'Uninstalled'
+        ret['comment'] = 'Plugin was successfully uninstalled.'
+    else:
+        ret['result'] = False
+        ret['comment'] = 'Could not uninstall plugin.'
+    return ret
+
+def installed(name, url, es_home=None):
+    '''
+    Make sure that a plugin is installed.
+
+    name
+        The name of the plugin to install
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+    if __opts__['test']:
+        ret['comment'] = 'The plugin {0} would have been installed'.format(name)
+        return ret
+
+    if name in __salt__['elasticsearch_plugins.list'](es_home):
+        ret['result'] = True
+        ret['comment'] = 'Plugin is already installed.'
+        return ret
+
+    __salt__['elasticsearch_plugins.install'](url, es_home)
+    if name in __salt__['elasticsearch_plugins.list'](es_home):
+        ret['result'] = True
+        ret['changes'][name] = 'Installed'
+        ret['comment'] = 'Plugin was successfully installed.'
+    else:
+        ret['result'] = False
+        ret['comment'] = 'Could not install plugin, is that a good URL?'
+    return ret

6644cd9fa112425892ef9be5ccbcaaac5ad8103c
Bruno Clermont <bruno.clermont@gmail.com>
2012-11-25 02:23:01 +0800

    add npm module and state

---
+++ states/_modules/npm.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+'''
+RabbitMQ plugins module
+'''
+
+import logging
+import json
+
+from salt import exceptions, utils
+
+log = logging.getLogger(__name__)
+
+def __virtual__():
+    '''
+    Verify npm is installed.
+    '''
+    try:
+        utils.check_or_die('npm')
+        return 'npm'
+    except exceptions.CommandNotFoundError:
+        log.error("Can't find npm")
+        return False
+
+def _npm(command, is_global=True, runas=None):
+    if is_global:
+        global_flag = ' -g '
+    else:
+        global_flag = ' '
+    if not runas:
+        runas = 'root'
+    user_home = __salt__['user.info'](runas)['home']
+    return __salt__['cmd.run']('npm' + global_flag + command,
+                               runas='root', env={'HOME': user_home})
+
+def list(is_global=True, runas=None):
+    '''
+    Return list of installed packages
+    '''
+    was_json = _npm('get json', is_global, runas) == 'true'
+    if not was_json:
+        _npm('set json=true', is_global, runas)
+    json_s = _npm('list', is_global, runas)
+    if not was_json:
+        _npm('set json=false', is_global, runas)
+    try:
+        output = json.loads(json_s)['dependencies']
+        log.info("json: '%s' output '%s'", json_s, output)
+        return output
+    except ValueError:
+        if json_s:
+            log.error("output of list is invalid: '%s'", json_s)
+        return {}
+
+def install(package, is_global=True, runas=None):
+    return _npm('install {0}'.format(package), is_global, runas)
+
+def uninstall(package, is_global=True, runas=None):
+    return _npm('uninstall {0}'.format(package), is_global, runas)
---
+++ states/_states/npm.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# author: Bruno Clermont <patate@fastmail.cn>
+
+'''
+NodeJS packages state
+'''
+
+from salt import exceptions, utils
+
+def __virtual__():
+    '''
+    Verify npm is installed.
+    '''
+    try:
+        utils.check_or_die('npm')
+        return 'npm'
+    except exceptions.CommandNotFoundError:
+        return False
+
+def uninstalled(name, is_global=True, runas=None):
+    '''
+    Make sure that an npm package is not uninstalled.
+
+    .. code-block:: yaml
+
+    less:
+      npm:
+        - uninstalled
+
+    name
+        The name of the package to uninstall
+
+    is_global
+        If the package is uninstalled globaly or for a single user only.
+        By default it's global.
+
+    run_as
+        User which run the command, Minion user by default.
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+    if __opts__['test']:
+        ret['comment'] = 'The package {0} would have been uninstalled'.format(
+            name)
+        return ret
+
+    packages = __salt__['npm.list'](is_global, runas)
+    if name not in packages:
+        ret['result'] = True
+        ret['comment'] = 'Package is not installed.'
+        return ret
+
+    __salt__['npm.uninstall'](name, is_global, runas)
+    packages = __salt__['npm.list'](is_global, runas)
+    if name not in packages:
+        ret['result'] = True
+        ret['changes'][name] = 'Uninstalled'
+        ret['comment'] = 'Package was successfully uninstalled.'
+    else:
+        ret['result'] = False
+        ret['comment'] = 'Package was not uninstalled.'
+
+    return ret
+
+def installed(name, is_global=True, runas=None):
+    '''
+    Make sure that an npm package is installed.
+
+    name
+        The name of the package to enable
+
+
+    is_global
+        If the package is installed globaly or for a single user only.
+        By default it's global.
+
+    run_as
+        User which run the command, Minion user by default.
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+
+    if __opts__['test']:
+        ret['comment'] = 'The package {0} would have been installed'.format(
+            name)
+        return ret
+
+    packages = __salt__['npm.list'](is_global, runas)
+    if name in packages:
+        ret['result'] = True
+        ret['comment'] = 'Package is already installed.'
+        return ret
+
+    __salt__['npm.install'](name, is_global, runas)
+    packages = __salt__['npm.list'](is_global, runas)
+    if name in packages:
+        ret['result'] = True
+        ret['changes'][name] = 'Installed'
+        ret['comment'] = 'Package was successfully installed.'
+    else:
+        ret['result'] = False
+        ret['comment'] = 'Package was not installed.'
+    return ret

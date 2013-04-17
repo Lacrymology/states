3a7af7dab2eb72fdbea7f35600be2a579137ad3c
Bruno Clermont <patate@fastmail.cn>
2013-04-17 21:50:55 +0800

    add a little warning

--- states/salt/bootstrap_minion.sh
+++ states/salt/bootstrap_minion.sh
@@ -3,3 +3,12 @@
 apt-add-repository -y ppa:saltstack/salt
 apt-get update
 apt-get install -y salt-minion
+
+rm -f /etc/salt/minion
+touch /etc/salt/minion
+
+echo "Please add the following lines to /etc/salt/minion and run: 'restart salt-minion':"
+echo
+echo 'master: $IPSALTSERVER'
+echo 'id: $HOSTNAME'
+echo

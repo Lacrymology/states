6ca416e06e9d1882bf947bbc06061d04d83ac4c6
Bruno Clermont <patate@fastmail.cn>
2013-06-03 19:12:41 +0800

    use salt mine to publish monitoring data from minion to master

---
+++ _modules/monitoring.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+def data():
+    '''
+    Return data specific for this minion required for monitoring.
+    '''
+    output = {
+        'shinken_pollers': __salt__['pillar.get']('shinken_pollers', []),
+        'roles': __salt__['pillar.get']('roles', []),
+    }
+
+    # figure how monitoring can reach this host
+    if 'availabilityZone' in __salt__['grains.ls']():
+        output['amazon_ec2'] = {
+            'availability_zone': __salt__['grains.get']('availabilityZone'),
+            'region':  __salt__['grains.get']('region')
+        }
+        output['ip_addrs'] = {
+            'public': [__salt__['grains.get']('public-ipv4')],
+            'private': [__salt__['grains.get']('privateIp')],
+        }
+    else:
+        # for now, just use eth0
+        output['ip_addrs'] = {
+            'public': __salt__['network.ip_addrs']('eth0')
+        }
+        if not output['ip_addrs']['public']:
+            # if nothing was found, just grab all IP address
+            output['ip_addrs']['public'] = __salt__['network.ip_addrs']()
+        output['ip_addrs']['private'] = output['ip_addrs']['public']
+

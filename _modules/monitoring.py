1ce3d9b1def7573c11ebba223ef135ca4daac6f6
Bruno Clermont <patate@fastmail.cn>
2013-06-20 18:11:08 +0800

    add flexible way to return more data

--- _modules/monitoring.py
+++ _modules/monitoring.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class _DontExistData(object):
+    pass
+
+
 def data():
     '''
     Return data specific for this minion required for monitoring.
@@ -28,5 +37,33 @@ def data():
             # if nothing was found, just grab all IP address
             output['ip_addrs']['public'] = __salt__['network.ip_addrs']()
         output['ip_addrs']['private'] = output['ip_addrs']['public']
+
+    # check monitoring_data pillar for extra values to return
+    monitoring_data = __salt__['pillar.get']('monitoring_data', {})
+    extra_data = {}
+    for key_name in monitoring_data:
+        try:
+            key_type = monitoring_data[key_name]['type']
+        except KeyError:
+            logger.error("Missing type for '%s'", key_name)
+            continue
+
+        try:
+            path = monitoring_data[key_name]['path']
+        except KeyError:
+            logger.error("Missing path for '%s'", key_name)
+            continue
+
+        if key_type == 'keys':
+            extra_data[key_name] = __salt__['pillar.get'](path, {}).keys()
+        elif key_type == 'exists':
+            value = __salt__['pillar.get'](path, _DontExistData)
+            if value == _DontExistData:
+                extra_data[key_name] = False
+            else:
+                extra_data[key_name] = True
+        else:
+            logger.error()
+    output['data'] = extra_data
 
     return output

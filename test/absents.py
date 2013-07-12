87ff37751c609b4ef2c2c66c906506aa26dbaba5
hvnsweeting <hvnsweeting@gmail.com>
2013-07-12 13:29:01 +0700

    refactor

--- test/absents.py
+++ test/absents.py
@@ -2,19 +2,35 @@
 # coding: utf-8
 
 import os
+import itertools
 
 
-def get_absent_states(common_path='..'):
-    '''Return list of all absent states inside `common_path` directory.'''
+def get_absent_states(filters=None, common_path='..'):
+    if filters is None:
+        filters = ['.sls']
+    '''Return list of all `expect` states inside `common_path` directory.
+    `expect`, for example,  can be 'absent.sls' or 'init.sls' ...
+
+    '''
     all_files = []
-    for root, dirs, files in os.walk(common_path):
-        li = [os.path.join(root, f) for f in files if '.git' not in root]
-        all_files.extend([path for path in li if "absent" in path])
+    for root, _, files in os.walk(common_path):
+        li = [os.path.join(root, f) for f in files]
+        all_files.extend([path for path in li
+                         if all(itertools.imap(lambda x, y: x in y, filters, itertools.repeat(f)))])
 
-    return sorted([f.replace('/', '.').rstrip('.sls').lstrip('.')
-                  for f in all_files])
+    # format output to saltstack DSL
+    all_files = [f.replace('/', '.')[:f.rfind('.sls')].lstrip('.')
+                 for f in all_files
+                 if not f.startswith('.git') and not f.startswith('doc.')]
+
+    return sorted(all_files)
 
 
 if __name__ == "__main__":
+    print 'all:', 10 * '*'
     for i in get_absent_states():
+        print i
+
+    print 'absent', 10 * '*'
+    for i in get_absent_states(['absent']):
         print i

591f45c24b4b1fd86779dc97e536a4e620fa51ed
Bruno Clermont <patate@fastmail.cn>
2013-09-08 01:08:34 +0800

    convert retcode_check.py to handle run_all

--- salt/ci/retcode_check.py
+++ salt/ci/retcode_check.py
@@ -2,19 +2,34 @@
 # -*- coding: utf-8 -*-
 
 """
-Take output of salt cmd.retcode --out json
+Take output of salt cmd.run_all --out json
 and sys.exit it's value
 
 This is only used to make a jenkins job fail if something bad happened
-during cmd.run
+during execution
 """
 
 import json
 import sys
+import os
 
 data = json.load(sys.stdin)
 keys = data.keys()
 if len(keys) != 1:
     print 'More than 1 key: %d: %s' % (len(data), keys)
 
-sys.exit(data[keys[0]])
+result = data[keys[0]]
+
+
+def write_output(output_type):
+    handler = getattr(sys, output_type)
+    handler.write(output_type.upper())
+    handler.write(os.linesep)
+    handler.write("-" * 79)
+    handler.write(os.linesep)
+    handler.write(result[output_type])
+    handler.write(os.linesep)
+
+write_output('stdout')
+write_output('stderr')
+sys.exit(result['retcode'])

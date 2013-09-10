ede49ffb2acaeed7e9ffc23bd4d5059cb56b1350
Bruno Clermont <patate@fastmail.cn>
2013-09-10 23:35:02 +0800

    process each download file one per one

--- salt/ci/import_test_data.py
+++ salt/ci/import_test_data.py
@@ -11,19 +11,19 @@ import sys
 import pwd
 
 
-def move_file(source_basename, destination_basename, source_dir, workspace,
+def move_file(basename, source_dir, workspace,
               user):
-    source_filename = os.path.join(source_dir, source_basename)
+    source_filename = os.path.join(source_dir, basename)
     if not os.path.exists(source_filename):
-        raise OSError("Can't find %s file %s" % (source_basename,
+        raise OSError("Can't find %s file %s" % (basename,
                                                  source_filename))
-    destination_filename = os.path.join(workspace, destination_basename)
+    destination_filename = os.path.join(workspace, basename)
     os.rename(source_filename, destination_filename)
     os.chown(destination_filename, user.pw_uid, user.pw_gid)
 
 
-def move_logs(job_id, job_name, minion_id, workspace, username='jenkins',
-              remote_dir='/root/salt',
+def move_logs(basename, minion_id, workspace,
+              username='jenkins', remote_dir='/root/salt',
               minions_dir='/var/cache/salt/master/minions'):
     try:
         user = pwd.getpwnam(username)
@@ -46,26 +46,17 @@ def move_logs(job_id, job_name, minion_id, workspace, username='jenkins',
         raise OSError("minion %s didn't pushed log files yet, can't find %s" % (
             minion_id, source_dir))
 
-    for log_type in ('stdout', 'stderr'):
-        move_file(
-            '{0}.log.xz'.format(log_type),
-            '{0}-{1}.log.xz'.format(job_id, log_type),
-            source_dir, workspace, user
-        )
-    move_file('TEST-States.xml', 'test-reports.xml',
-              source_dir, workspace, user)
+    move_file(basename, source_dir, workspace, user)
 
 
 def main():
-    if len(sys.argv) != 5:
-        print 'Invalid arguments, syntax: %s [job-id] [job-name] ' \
-              '[minion-id] [workspace]' % sys.argv[0]
+    if len(sys.argv) != 4:
+        print 'Invalid arguments, syntax: %s [file] [minion-id] [workspace]'\
+              % sys.argv[0]
         sys.exit(1)
 
-    job_id, job_name, minion_id, workspace = sys.argv[1:5]
-
     try:
-        move_logs(job_id, job_name, minion_id, workspace)
+        move_logs(*sys.argv[1:4])
     except Exception, err:
         print str(err)
         sys.exit(1)

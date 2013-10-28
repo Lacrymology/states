e84a3e2860c9993c7fc4a1f6a9be84e7a9c9a5c6
hvnsweeting <hvnsweeting@gmail.com>
2013-10-28 16:39:19 +0700

    t1484: FIX wrong state name in example

--- _states/dnsimple.py
+++ _states/dnsimple.py
@@ -5,13 +5,13 @@ Copyright (c) 2013, Bruno Clermont
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met: 
+modification, are permitted provided that the following conditions are met:
 
 1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer. 
+   list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution. 
+   and/or other materials provided with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -70,7 +70,7 @@ def created(name, email, token):
     sls example
 
     example.com:
-      dnssimple:
+      dnsimple:
         - registered
         - email: xxx
         - token: xxx
@@ -136,7 +136,7 @@ def records_exists(name, email, token, records):
     sls example
 
     state_name:
-      dnssimple.records_exists:
+      dnsimple.records_exists:
       - email: xxx
       - token: xxx
       - records:

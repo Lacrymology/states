2287564625db65f1133d41a97a16568327039b56
Bruno Clermont <patate@fastmail.cn>
2014-06-10 14:30:43 +0800

    simplify and cleanup

--- denyhosts/denyhosts-unblock.py
+++ denyhosts/denyhosts-unblock.py
@@ -33,98 +33,64 @@ __author__ = 'Diep Pham'
 __maintainer__ = 'Diep Pham'
 __email__ = 'favadi@robotinfra.com'
 
-import fileinput
 import sys
 import subprocess
+import logging
 
+logger = logging.getLogger(__name__)
 
-def delete_matching_line(pattern, files):
-    """
-    Backup all file in files with .bak extension, delete all line
-    contain given pattern.
-
-    Args:
-      pattern (str): a regular expression
-      files (list): list of filename to delete matching line
-
-    """
-    for line in fileinput.input(files, inplace=1, backup='.bak'):
-        if pattern not in line:
-            print line,
-        else:
-            sys.stderr.write(
-                'line: {} remove {} from file {}\n'.format(
-                    fileinput.filelineno(), pattern, fileinput.filename()))
-
-
-def stop_start_service(service, action):
-    """
-    start or stop service
-
-    Args:
-      service (str): path to init script file
-      action (str): 'stop' or 'start'
-
-    Return:
-      0: stop or start service successfully
-      -1: invalid action
-      int > 0: stop or start fail, return code
-
-    """
-    # invalid action
-    if action not in ['stop', 'start']:
-        return False
-
-    print '{} denyhosts'.format(action)
-
-    try:
-        subprocess.check_call([service, action])
-        return 0  # success
-    except subprocess.CalledProcessError as err:
-        print '{} denyhosts: failed (err: {})'.format(action, err.returncode)
-        return err.returncode  # fail
-    except OSError as err:
-        print '{} is not found (err: {})'.format(service, err.errno)
-        return err.errno  # file not found
+def filter_ips(filename, ips):
+    lines = []
+    is_black = False
+    open(filename) as fh:
+        for line in fh:
+            line_is_black = False
+            for ip in sys.argv[1:]:
+                if ip in line:
+                    logger.debug("Found IP %s in '%s' line '%s'.",
+                                 filename, ip, line.rstrip(os.linesep))
+                    line_is_black = is_black = True
+                    break
+            if not line_is_black:
+                lines.append(line)
+    if is_black:
+        logger.debug("Overwrite content of %s.", filename)
+        open(filename, 'w') as fh:
+            fh.writelines(lines)
+    else:
+        logger.debug("File %s don't had any changes, leave as is.", filename)
 
 
 def main():
     # list of denyhosts files
-    denyhosts_files = [
+    denyhosts_files = (
         '/etc/hosts.deny',
         '/var/lib/denyhosts/hosts',
         '/var/lib/denyhosts/hosts-restricted',
         '/var/lib/denyhosts/hosts-root',
         '/var/lib/denyhosts/hosts-valid',
         '/var/lib/denyhosts/users-hosts',
-    ]
-
-    # usage message
-    usage = 'usage: denyhosts-unblock <ip_address> ...'
+    )
 
     # get list of arguments, each arg is one ip address to unban
-    args = sys.argv[1:]
+    ips = sys.argv[1:]
 
     # need one or more arg
-    if args:
-        # stop denyhosts
-        stop_err = stop_start_service('/etc/init.d/denyhosts', 'stop')
-        if stop_err > 0:
-            sys.exit(stop_err)
+    if ips:
+        init_script = '/etc/init.d/denyhosts'
+        logger.debug("stop %s", init_script)
+        subprocess.check_call([init_script, 'stop'])
 
-        # delete matching ip
-        for arg in args:
-            delete_matching_line(arg, denyhosts_files)
+        for filename in denyhosts_files:
+            filter_ips(filename, ips)
 
         # start denyhosts
-        start_err = stop_start_service('/etc/init.d/denyhosts', 'start')
-        if start_err > 0:
-            sys.exit(start_err)
-
+        logger.debug("start %s", init_script)
+        subprocess.check_call([init_script, 'start'])
     else:
-        # print help message if empty arguments
-        print usage
+        print 'usage: denyhosts-unblock <ip_address> ...'
 
 
 if __name__ == '__main__':
+    logger.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     main()

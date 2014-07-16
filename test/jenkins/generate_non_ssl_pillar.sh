bc4186607c050f57b45b0cb8358bfa677b3ba704
Hung Nguyen Viet <hvnsweeting@gmail.com>
2014-07-16 11:09:04 +0700

    erpt1242: add document, change all .sls files

--- test/jenkins/generate_non_ssl_pillar.sh
+++ test/jenkins/generate_non_ssl_pillar.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# script to removes all SSL relate stuff from files locate into input dir
+# this will change the origin pillar files, make it become non-SSL pillar,
+# which used to test formulas with non-SSL.
 pillar_dir=$1
 cd $pillar_dir
-sed -i 's/  ssl_redirect:.*//g' integration.sls
-sed -i 's/  ssl:.*//g' integration.sls
+sed -i 's/  ssl_redirect:.*//g' *.sls
+sed -i 's/  ssl:.*//g' *.sls
 # show changes for debugging purpose
 git diff

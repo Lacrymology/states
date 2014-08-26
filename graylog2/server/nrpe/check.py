842894cb3d664200a1dcf979bcc4308b032dd6b1
Diep Pham <favadi@robotinfra.com>
2014-08-26 15:26:34 +0700

    erp1518: use bfs.nrpe for check_new_logs

--- graylog2/server/nrpe/check.py
+++ graylog2/server/nrpe/check.py
@@ -3,26 +3,30 @@
 
 # Copyright (c) 2013, Bruno Clermont
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 """
 Nagios plugin to check if new log messages indexed by Graylog2 server.
@@ -32,39 +36,56 @@ __author__ = 'Bruno Clermont'
 __maintainer__ = 'Diep Pham'
 __email__ = 'favadi@robotinfra.com'
 
-import argparse
+import logging
+import bfs.nrpe as bfe
 import nagiosplugin
 import requests
+
+log = logging.getLogger('nagiosplugin')
 
 
 class Graylog2Throughput(nagiosplugin.Resource):
 
     def __init__(self, api_url, username, password):
-
-        throughput_url = '/system/throughput'
-
-        r = requests.get(
-            api_url + throughput_url, auth=(username, password))
-        self._throughput = r.json()['throughput']
+        self._api_url = api_url
+        self._username = username
+        self._password = password
 
     def probe(self):
-        return nagiosplugin.Metric('throughput', self._throughput, min=0)
+        try:
+            # /system/throughput sometimes return 0 (which is normal),
+            # retry 5 times before return 0
+            for _ in xrange(5):
+                r = requests.get(
+                    self._api_url, auth=(self._username, self._password))
+                throughput = r.json['throughput']
+                if throughput != 0:
+                    break
+        except requests.ConnectionError:
+            raise nagiosplugin.CheckError(
+                'Could not connect to graylog2 server: {}'.format(
+                    self._api_url))
+        except ValueError:
+            raise nagiosplugin.CheckError(
+                'Invalid response from graylog2 server: {}'.format(
+                    self._api_url))
+
+        return [
+            nagiosplugin.Metric('throughput', int(throughput), min=0)
+        ]
 
 
 @nagiosplugin.guarded
 def main():
-    argp = argparse.ArgumentParser(description=__doc__)
-    argp.add_argument('-w', '--warning', metavar='RANGE', default='1:10000',
-                      help='critical if message numbers is outside RANGE')
+    argp = bfe.ArgumentParser(description=__doc__)
     args = argp.parse_args()
-    throughput = Graylog2Throughput(
-        'http://127.0.0.1:12900/system/throughput',
-        "{{ salt['pillar.get']('graylog2:root_username', 'admin') }}",
-        "{{ pillar['graylog2']['admin_password'] }}")
+    config = bfe.ConfigFile.from_arguments(args)
+    kwargs = config.kwargs('api_url', 'username', 'password')
+    crit_range = config.get_argument('crit_range', '1:10000')
     check = nagiosplugin.Check(
-        throughput,
-        nagiosplugin.ScalarContext('throughput', args.warning, args.warning))
-    check.main()
+        Graylog2Throughput(**kwargs),
+        nagiosplugin.ScalarContext('throughput', critical=crit_range))
+    check.main(args.verbose)
 
 
 if __name__ == '__main__':

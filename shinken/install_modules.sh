c9e72cb2b00015a4cdb56b5e24b99b8a0f7f5db6
quanta <tonganhquan.net@gmail.com>
2014-09-12 13:46:45 +0700

    erpt928: `shinken` cli should be run with a correct python path

--- shinken/install_modules.sh
+++ shinken/install_modules.sh
@@ -3,5 +3,5 @@
 modules=( "auth-cfg-password" "booster-nrpe" "graphite" "nsca" "pickle-retention-file-generic" "sqlitedb" "syslog-sink" "ui-graphite" "webui" )
 for module in "${modules[@]}"
 do
-    /usr/local/shinken/bin/shinken install $module
+    /usr/local/shinken/bin/python /usr/local/shinken/bin/shinken install $module
 done

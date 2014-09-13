787f912bbe8dc8547c747df06e81ddda9eb15494
Tomas Neme <lacrymology@gmail.com>
2014-09-13 16:53:22 +0700

    erp470: create first try for the raven module.

---
+++ _modules/raven.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014, Tomas Neme
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+raven (sentry client) module.
+"""
+
+__author__ = 'Tomas Neme'
+__maintainer__ = 'Tomas Neme'
+__email__ = 'lacrymology@gmail.com'
+
+import logging
+try:
+    import raven
+except ImportError:
+    raven = False
+
+log = logging.getLogger(__name__)
+
+def __virtual__():
+    if raven:
+        return 'raven'
+
+def alert(dsn, message, level='INFO', extra=None):
+    """
+    Send a sentry alert
+
+    :param dsn: The sentry DSN, in the form of
+        {protocol}://{public}:{private}@{host}/{path}{project id}
+    :param message: The message string you want to send
+    :param level: The level of the message (DEBUG, INFO, WARN, ERROR)
+    :param extra: Any extra parameters you want sent with the message
+    """
+    if extra is None:
+        extra = {}
+    client = raven.Client(dsn)
+    level = getattr(logging, level.upper(), 'INFO')
+    extra.update({'level': level})
+    data = client.build_msg('raven.events.Message', message=message, data=extra)
+    client.send(**data)
+
+    return {
+        'message': data
+    }

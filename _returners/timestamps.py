ce91bd5665c921fd0983d24612f5efcfb526982f
Hung Nguyen Viet <hvnsweeting@gmail.com>
2014-10-01 18:33:25 +0700

    erpt1651: add

---
+++ _returners/timestamps.py
@@ -0,0 +1,26 @@
+#! -*- coding: utf-8 -*-
+'''
+Writes timestamps to a file if ret data is the one of success.
+'''
+
+import os
+import time
+import logging
+
+
+log = logging.getLogger()
+__virtualname__ = 'timestamps'
+TS_PATH = os.path.join('/var/cache/salt/minion', 'returner_timestamps')
+
+
+def __virtual__():
+    return __virtualname__
+
+
+def returner(ret):
+    success = all(ret['return'][state]['result']
+                  for state in ret['return'])
+    if success:
+        log.info('Did this %s run success? %s', ret['fun'], str(success))
+        with open(TS_PATH, 'w+') as f:
+            f.write(str(time.time()))

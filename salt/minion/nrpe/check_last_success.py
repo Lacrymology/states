7561762097aba538f72ffd7f395c91ca6930297f
Hung Nguyen Viet <hvnsweeting@gmail.com>
2014-10-03 16:20:39 +0700

    erpt1651: use timestamps from minion datastore
    as in returner, it was changed to set value there

--- salt/minion/nrpe/check_last_success.py
+++ salt/minion/nrpe/check_last_success.py
@@ -1,36 +1,41 @@
 import datetime
 import logging
 
+import salt.config
+import salt.loader
 import nagiosplugin as nap
-import yaml
 
 import pysc
 import pysc.nrpe as bfe
 
-
 log = logging.getLogger('nagiosplugin.salt.minion.last_success')
-timestamps_path = '/var/cache/salt/minion/returner_timestamps'
+TS_KEY = 'returner_timestamps_last_success'
 
 
 class LastSuccess(nap.Resource):
     def probe(self):
         try:
-            with open(timestamps_path) as f:
-                ts = yaml.load(f)
-                try:
-                    hours = (datetime.datetime.now() -
-                             datetime.datetime.strptime(ts['last_success'],
-                             "%Y-%m-%dT%H:%M:%S.%f")).total_seconds() / 3600
-                    ret = [nap.Metric('last_success', hours, min=0,
-                                      context='hours')]
-                    return ret
-                except Exception:
-                    log.critical('Not found timestamps of last success')
-                    raise
-        except IOError:
-            log.critical('%s does not exist, looks like ``timestamps``'
-                         ' returner has never been called')
+            __opts__ = salt.config.minion_config('/etc/salt/minion')
+            datamod = salt.loader.raw_mod(__opts__, 'data', None)
+            ts = datamod['data.getval'](TS_KEY)
+        except Exception as e:
+            log.critical('Cannot get value of %s. Error: %s',
+                         TS_KEY, e, exc_info=True)
             raise
+        else:
+            try:
+                hours = (datetime.datetime.now() -
+                         datetime.datetime.strptime(ts,
+                         "%Y-%m-%dT%H:%M:%S.%f")).total_seconds() / 3600
+                ret = [nap.Metric('last_success', hours, min=0,
+                                  context='hours')]
+                return ret
+            except Exception:
+                log.critical(('Expected a string presents time in ISO format, '
+                              'got %r. If it is None, probably timestamps '
+                              'returner has never returned.'),
+                             ts)
+                raise
 
 
 @nap.guarded

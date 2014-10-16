769070a7aa2782a2eb08551f36584996f380fdbf
Hung Nguyen Viet <hvnsweeting@gmail.com>
2014-10-16 10:48:11 +0700

    erpt1663: add lint check for number of order: last arg
    and split existing code to print_grep_result func

--- test/lint.py
+++ test/lint.py
@@ -20,12 +20,14 @@ def _grep(paths, pattern):
         if found:
             all_found[path] = found
 
+    return all_found
+
+
+def _print_grep_result(all_found):
     for fn in all_found:
         print fn
         for line in all_found[fn]:
-            print line
-
-    return all_found
+            print ' ', line
 
 
 def _print_tips(content):
@@ -36,6 +38,20 @@ def lint_check_tab_char(paths):
     found = _grep(paths, '\t')
     if found:
         _print_tips('Must use spaces instead of tab char')
+        _print_grep_result(found)
+        return False
+    return True
+
+
+def lint_check_numbers_of_order_last(paths):
+    found = _grep(paths, '- order: last')
+    many_last = {k: v for k, v in found.iteritems() if len(v) == 2}
+
+    if many_last:
+        _print_tips("Only one '- order: last' takes effect, use only one"
+                    " of that and replace other with specific requisite"
+                    " (you may want to require ``sls: sls_file`` instead)")
+        _print_grep_result(many_last)
         return False
     return True
 
@@ -60,5 +76,6 @@ if __name__ == "__main__":
     paths = process_args()
     res = []
     res.append(lint_check_tab_char(paths))
+    res.append(lint_check_numbers_of_order_last(paths))
     no_of_false = [i for i in res if i is False]
     sys.exit(no_of_false)

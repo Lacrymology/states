d4557ce7337d167fef195196d733bfc1afff45bb
Hung Nguyen Viet <hvnsweeting@gmail.com>
2014-11-04 22:28:19 +0700

    erpt1610: use variables for all hardcoded paths

--- test/jenkins/build.sh
+++ test/jenkins/build.sh
@@ -81,35 +81,40 @@ sudo salt $BUILD_IDENTITY test.ping | grep True
 sudo salt -t 10 "$BUILD_IDENTITY" cmd.run "hostname $BUILD_IDENTITY"
 sudo salt -t 60 "$BUILD_IDENTITY" cp.get_file salt://jenkins_archives/$BUILD_IDENTITY.tar.gz /tmp/bootstrap-archive.tar.gz
 sudo salt -t 60 "$BUILD_IDENTITY" archive.tar xzf /tmp/bootstrap-archive.tar.gz cwd=/
-master_ip=$(sudo salt -t 60 "$BUILD_IDENTITY" --out=yaml grains.item master | cut -f2- -d ':' | tr -d '\n')
+
+PREPARE_STDOUT_LOG=/root/salt/stdout.prepare
+PREPARE_STDERR_LOG=/root/salt/stderr.prepare
+CUSTOM_CONFIG_DIR=/root/salt/states/test
 
 function run_and_check_return_code {
     sudo salt -t $1 "$BUILD_IDENTITY" --output json cmd.run_all "$2" | ./test/jenkins/retcode_check.py
 }
-run_and_check_return_code 10 "sed -i \"s/master:.*/master: $master_ip/g\" /root/salt/states/test/minion"
+
+master_ip=$(sudo salt -t 60 "$BUILD_IDENTITY" --out=yaml grains.item master | cut -f2- -d ':' | tr -d '\n')
+run_and_check_return_code 10 "sed -i \"s/master:.*/master: $master_ip/g\" $CUSTOM_CONFIG_DIR/minion"
 # sync extmod from extracted archive to bootstrapped salt instance
-run_and_check_return_code 60 "salt-call -c /root/salt/states/test/ saltutil.sync_all >> /root/salt/stdout.prepare 2>> /root/salt/stderr.prepare"
-run_and_check_return_code 600 "salt-call -c /root/salt/states/test/ state.sls test.sync >> /root/salt/stdout.prepare 2>> /root/salt/stderr.prepare"
-run_and_check_return_code 600 "salt-call -c /root/salt/states/test/ state.sls test.jenkins >> /root/salt/stdout.prepare 2>> /root/salt/stderr.prepare"
+run_and_check_return_code 60 "salt-call -c $CUSTOM_CONFIG_DIR saltutil.sync_all >> $PREPARE_STDOUT_LOG 2>> $PREPARE_STDERR_LOG"
+run_and_check_return_code 600 "salt-call -c $CUSTOM_CONFIG_DIR state.sls test.sync >> $PREPARE_STDOUT_LOG 2>> $PREPARE_STDERR_LOG"
+run_and_check_return_code 600 "salt-call -c $CUSTOM_CONFIG_DIR state.sls test.jenkins >> $PREPARE_STDOUT_LOG 2>> $PREPARE_STDERR_LOG"
 echo '------------ From here, salt with version supported by salt-common is running ------------'
 sudo salt -t 5 "$BUILD_IDENTITY" --output json cmd.run_all "salt-call test.ping"
-run_and_check_return_code 10 "salt-call -c /root/salt/states/test/ state.sls salt.patch_salt >> /root/salt/stdout.prepare 2>> /root/salt/stderr.prepare"
-run_and_check_return_code 20 "salt-call -c /root/salt/states/test/ saltutil.sync_all >> /root/salt/stdout.prepare 2>> /root/salt/stderr.prepare"
-run_and_check_return_code 10 "salt-call -c /root/salt/states/test/ saltutil.refresh_modules >> /root/salt/stdout.prepare 2>> /root/salt/stderr.prepare"
+run_and_check_return_code 10 "salt-call -c $CUSTOM_CONFIG_DIR state.sls salt.patch_salt >> $PREPARE_STDOUT_LOG 2>> $PREPARE_STDERR_LOG"
+run_and_check_return_code 20 "salt-call -c $CUSTOM_CONFIG_DIR saltutil.sync_all >> $PREPARE_STDOUT_LOG 2>> $PREPARE_STDERR_LOG"
+run_and_check_return_code 10 "salt-call -c $CUSTOM_CONFIG_DIR saltutil.refresh_modules >> $PREPARE_STDOUT_LOG 2>> $PREPARE_STDERR_LOG"
 
 start_run_test_time=$(date +%s)
 echo "TIME-METER: Preparing for test took: $((start_run_test_time - start_time)) seconds"
 echo '------------ Running CI test  ------------'
-sudo salt -t 86400 "$BUILD_IDENTITY" cmd.run "/root/salt/states/test/jenkins/run.py $*"
+sudo salt -t 86400 "$BUILD_IDENTITY" cmd.run "$CUSTOM_CONFIG_DIR/jenkins/run.py $*"
 finish_run_test_time=$(date +%s)
 echo "TIME-METER: Run integration.py took: $((finish_run_test_time - start_run_test_time)) seconds"
 
 for ltype in stdout stderr; do
-    run_and_check_return_code 30 "salt-call -c /root/salt/states/test/ cmd.run \"xz -c /root/salt/$ltype.prepare > /tmp/$BUILD_IDENTITY-$ltype.prepare.log.xz\""
-    run_and_check_return_code 30 "salt-call -c /root/salt/states/test/ cmd.run \"xz -c /root/salt/$ltype.log > /tmp/$BUILD_IDENTITY-$ltype.log.xz\""
+    run_and_check_return_code 30 "salt-call -c $CUSTOM_CONFIG_DIR cmd.run \"xz -c /root/salt/$ltype.prepare > /tmp/$BUILD_IDENTITY-$ltype.prepare.log.xz\""
+    run_and_check_return_code 30 "salt-call -c $CUSTOM_CONFIG_DIR cmd.run \"xz -c /root/salt/$ltype.log > /tmp/$BUILD_IDENTITY-$ltype.log.xz\""
 done
 
-sudo salt -t 60 "$BUILD_IDENTITY" --output json cmd.run_all "salt-call -l info -c /root/salt/states/test/ state.sls test.jenkins.result"
+sudo salt -t 60 "$BUILD_IDENTITY" --output json cmd.run_all "salt-call -l info -c $CUSTOM_CONFIG_DIR state.sls test.jenkins.result"
 
 xz -d -c /home/ci-agent/$BUILD_IDENTITY-stderr.log.xz
 cp /home/ci-agent/$BUILD_IDENTITY-result.xml $WORKSPACE/result.xml

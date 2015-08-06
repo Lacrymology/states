5f537307e159b3507d033d2ccf2e65df3efda4cb
quanta <quanta@robotinfra.com>
2015-08-06 21:43:49 +0700

    erp3354: patch `dictupdate.py` to merge pillar list

--- salt/patch/dictupdate.py
+++ salt/patch/dictupdate.py
@@ -8,14 +8,46 @@ http://stackoverflow.com/a/3233356
 import collections
 
 
-def update(dest, upd):
-    for key, val in upd.iteritems():
-        if isinstance(val, collections.Mapping):
-            ret = update(dest.get(key, {}), val)
-            dest[key] = ret
-        elif key:
-            dest[key] = upd[key]
-    return dest
+def update(dest, upd, recursive_update=True):
+    '''
+    Recursive version of the default dict.update
+
+    Merges upd recursively into dest
+
+    If recursive_update=False, will use the classic dict.update, or fall back
+    on a manual merge (helpful for non-dict types like FunctionWrapper)
+    '''
+    if (not isinstance(dest, collections.Mapping)) \
+            or (not isinstance(upd, collections.Mapping)):
+        raise TypeError('Cannot update using non-dict types in dictupdate.update()')
+    updkeys = list(upd.keys())
+    if not set(list(dest.keys())) & set(updkeys):
+        recursive_update = False
+    if recursive_update:
+        for key in updkeys:
+            val = upd[key]
+            try:
+                dest_subkey = dest.get(key, None)
+            except AttributeError:
+                dest_subkey = None
+            if isinstance(dest_subkey, collections.Mapping) \
+                    and isinstance(val, collections.Mapping):
+                ret = update(dest_subkey, val)
+                dest[key] = ret
+            elif isinstance(dest_subkey, list) \
+                     and isinstance(val, list):
+                dest[key] = dest.get(key, []) + val
+            else:
+                dest[key] = upd[key]
+        return dest
+    else:
+        try:
+            dest.update(upd)
+        except AttributeError:
+            # this mapping is not a dict
+            for k in upd:
+                dest[k] = upd[k]
+        return dest
 
 
 def merge(d1, d2):

1d438740a0aba1864eb19cd96cec8118c7b17dfc
Diep Pham <favadi@fastmail.fm>
2015-08-24 16:52:36 +0700

    erp3583: add more rss debug info

--- test/jenkins/upgrade_salt.sh
+++ test/jenkins/upgrade_salt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -o DPkg::Options::=--force-confold -o DPkg::Options::=--force-confdef -y python-pip
+apt-get install -o DPkg::Options::=--force-confold -o DPkg::Options::=--force-confdef -y python-pip python-psutil
 pip install http://leo.robotinfra.com/mirror/unittest-xml-reporting-a4d6593eb9b85996021285cc2ca3830701fcfe9b.tar.gz
 apt-get install -o DPkg::Options::=--force-confold -o DPkg::Options::=--force-confdef -y salt-minion
 restart salt-minion || true

a1dd34ae60b24e473cb9b1474c655802f1bc54ea
Hung Nguyen Viet <hvn@robotinfra.com>
2015-12-17 14:31:59 +0800

    Minion should not have access to jenkins.
    Revert "ubaqa80: improve, use config from minion"
    This reverts commit b4e75bae306f1e4ba140be10effa8773714e0a35.

--- _modules/jenkins.py
+++ _modules/jenkins.py
@@ -2,15 +2,7 @@
 # Use of this is governed by a license that can be found in doc/license.rst.
 
 '''
-Module for interactive with Jenkins API
-
-:configuration: In order to connect to Jenkins, certain configuration is
-required in /etc/salt/minion on the relevant minions. Some sample configs might
-look like::
-
-  jenkins.base_url: https://jenkins.com
-  jenkins.username: meow
-  jenkins.token: some_token_get_from_meow_user
+Get latest build number of given Jenkins jobs
 '''
 
 __author__ = 'Viet Hung Nguyen <hvn@robotinfra.com>'
@@ -23,23 +15,16 @@ import requests
 logger = logging.getLogger(__name__)
 
 
-def latest_build(jobname):
+def latest_build(jenkins_addr, jobname, username, token):
     '''
     Access API of given jobname at jenkins_addr
-    and find out latest successful build number.
+    and find out latest build number
     '''
-    auth = (__salt__['config.option']('jenkins.username'),
-            __salt__['config.option']('jenkins.token'))
-    base_url = __salt__['config.option']('jenkins.base_url').rstrip('/')
-    if not base_url:
-        logger.error("Configuration: `jenkins.base_url` "
-                     "is required for jenkins module")
-        return
-    request_url = '{0}/job/{1}/api/json'.format(base_url, jobname)
-    logger.info("Connecting to %s", request_url)
+
+    request_url = 'https://{0}/job/{1}/api/json'.format(jenkins_addr, jobname)
     build_number = None
     try:
-        r = requests.get(request_url, auth=auth)
+        r = requests.post(request_url, auth=(username, token))
         build_number = r.json()['lastStableBuild'].get('number', None)
     except Exception as e:
         logger.error(e, exc_info=True)
